d925690a123fae2eb59b34a5fc915d5814daf13c
Duc Minh Nguyen <nguyenminhkhmt@gmail.com>
2023-04-09 19:30:53 +0300

    Init bloglist - part 12 fe & be

---
+++ bloglist-frontend/src/components/Message.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Alert } from 'react-bootstrap'
+
+const Message = ({ message }) => {
+  if (message === null || message === undefined) {
+    return (null)
+  }
+
+  return (
+    <Alert variant={message.type}>{message.body}</Alert>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    message: state.message
+  }
+}
+
+const connectedMessage = connect(
+  mapStateToProps
+)(Message)
+
+export default connectedMessage
